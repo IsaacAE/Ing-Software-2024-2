146028b6ddb069416b073a75dd5d95488811f4d9
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2024-02-24 20:55:32 -0600

    Cambiando Script2 para conectarse correctamente con la db y agregando el requirements.txt

--- Script2.py
+++ Script2.py
@@ -7,14 +7,15 @@ from alchemyClasses.peliculas import peliculas
 from model.model_peliculas import consultar_registros_peliculas, filtrar_id_pelicula, cambiar_columna_nombre_pelicula, eliminar_id_pelicula, eliminar_tabla_peliculas
 from model.model_usuarios import consultar_registros_usuarios, filtrar_id_usuario,cambiar_columna_nombre_usuario, eliminar_id_usuario, eliminar_tabla_usuarios
 from model.model_rentar import consultar_registros_rentas, filtrar_id_renta, cambiar_columna_fecha_renta, eliminar_id_renta, eliminar_tabla_rentar
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://lab:Developer123!@localhost:3306/lab_ing_software'
-app.config.from_mapping(
+Script2 = Flask(__name__)
+Script2.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://lab:Developer123!@localhost:3306/lab_ing_software'
+Script2.config.from_mapping(
     SECRET_KEY='dev'
 )
-db.init_app(app)
+db.init_app(Script2)
 
-def mostrar_menu():
+#Funcion para mostrar el menu de opciones al usuario
+def menu():
     print("### MENU ###")
     print("1. Consultar registros de una tabla.")
     print("2. Buscar registro de una tabla mediante el ID.")
@@ -22,6 +23,7 @@ def mostrar_menu():
     print("4. Eliminar un registro por ID o eliminar todos los registros de la tabla.")
     print("5. Salir")
 
+#Funcion que toma un String y lo convierte a una fecha de formato yyyy-mm-dd
 def convertir_string_a_fecha(fecha_str):
     try:
         fecha = datetime.datetime.strptime(fecha_str, '%Y-%m-%d')
@@ -30,6 +32,7 @@ def convertir_string_a_fecha(fecha_str):
         print("Oh, lo siento, pero el formato proporcionado no es valido.")
         return None
 
+#Funcion para que el usuario elija la tabla sobre la cual desea realizar la operacion
 def obtener_tabla_usuario():
     while True:
         try:
@@ -41,19 +44,21 @@ def obtener_tabla_usuario():
             elif opcion == 3:
                 return "rentar"
             else:
-                print("Por favor, ingrese un número válido (1, 2 o 3).")
+                print("Por favor, ingrese un numero valido (1, 2 o 3).")
         except ValueError:
-            print("Por favor, ingrese un número válido (1, 2 o 3).")    
+            print("Por favor, ingrese un numero valido (1, 2 o 3).")    
 
+
+#Funcion que ejecuta el main para que el usuario interactue con el programa y todas las operaciones que se pueden realizar
 if __name__ == '__main__':
-    with app.app_context():
+    with Script2.app_context():
         while True:
-            mostrar_menu()
+            menu()
 
             try:
-                opcion = int(input("Seleccione una opción (1-5): "))
+                opcion = int(input("Escribe el numero de la operacion que quieres hacer (1-5): "))
             except ValueError:
-                print("Error: Ingrese un número válido.")
+                print("Ese numero de operacion no es valido.")
                 continue
 
             if opcion == 1:
@@ -70,9 +75,9 @@ if __name__ == '__main__':
                 tabla = obtener_tabla_usuario()
 
                 try:
-                    id_buscar = input("Ingrese el ID a filtrar: ")
+                    id_buscar = input("Ingresa el ID con el que deseas buscar: ")
                 except ValueError:
-                    print("Error: Ingrese un ID válido.")
+                    print("Ese no es un ID valido.")
                     continue
 
                 if tabla == "peliculas":
@@ -88,24 +93,22 @@ if __name__ == '__main__':
                 try:
                     id_actualizar = int(input("Ingrese el ID del registro a actualizar: "))
                 except ValueError:
-                    print("Error: Ingrese un ID válido.")
+                    print("Ese ID no es valido.")
                     continue
 
-                nuevo_valor = input("Ingrese el nuevo valor: ")
+                nuevo_valor = input("Ingrese el nuevo valor (si es una fecha escribelo con el formato 'yyyy-mm-dd'): ")
                 if tabla == "peliculas":
                     cambiar_columna_nombre_pelicula(id_actualizar, nuevo_valor)
                 elif tabla == "usuarios":
                     cambiar_columna_nombre_usuario(id_actualizar, nuevo_valor)
                 elif tabla == "rentar":
-                    # Intentar convertir el nuevo valor en una fecha
+                    #Se intenta convertir el String a fecha si se selecciono la opcion para rentas
                     try:
                         nueva_fecha = convertir_string_a_fecha(nuevo_valor)
                         if nueva_fecha is not None:
-                            # Si la conversión es exitosa, llamar a la función para cambiar la fecha
                             cambiar_columna_fecha_renta(id_actualizar, nueva_fecha)
                     except ValueError:
-                        # Si la conversión falla, mostrar un mensaje de error
-                        print("Error: Ingrese una fecha válida en formato dd/mm/aaaa.")
+                        print("Dicho formato de fecha no es valido.")
                         continue
 
                 
@@ -117,7 +120,7 @@ if __name__ == '__main__':
                 try:
                     id_eliminar = input("Ingrese el ID del registro a eliminar (si no ingresa ningun ID, se borrara toda la tabla): ")
                 except ValueError:
-                    print("Error: Ingrese un ID válido.")
+                    print("Ese ID no es valido.")
                     continue
 
                 if tabla == "peliculas":
@@ -129,10 +132,9 @@ if __name__ == '__main__':
 
 
             elif opcion == 5:
-                print("Saliendo del programa. ¡Hasta luego!")
+                print("Saliendo del programa. Auf wiedersehen")
                 break
 
             else:
-                print("Opción no válida. Inténtelo de nuevo.")
+                print("No existe tal opcion de operacion.")
     
-     
