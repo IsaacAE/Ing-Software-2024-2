f451f9d8a2392c99a2a740289f08c2a99b4a1a04
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2024-03-12 23:19:32 -0600

    Haciendo CRUD para peliculas

--- flaskProject/model/model_peliculas.py
+++ flaskProject/model/model_peliculas.py
@@ -1,48 +1,35 @@
 from alchemyClasses.peliculas import peliculas
 from alchemyClasses import db
 
-def consultar_registros_peliculas():
-    for pelicula in peliculas.query.all():
-        print(pelicula)
+def crear_pelicula(nombre, genero=None, duracion=None, inventario=1):
+    nueva_pelicula = peliculas(nombre=nombre, genero=genero, duracion=duracion, inventario=inventario)
+    db.session.add(nueva_pelicula)
+    db.session.commit()
+    return nueva_pelicula
 
-def filtrar_id_pelicula(id):
-    pelicula = peliculas.query.filter_by(idPelicula=id).first()
-    if pelicula is None:
-        print("No existe pelicula con dicho ID.") 
-    else:
-        print(pelicula)
+def leer_peliculas():
+    return peliculas.query.all()
 
+# Función para obtener una película por su ID
+def leer_pelicula_por_id(id):
+    return peliculas.query.get(id)
 
-def cambiar_columna_nombre_pelicula(id_pelicula,nombre):
-    pelicula = peliculas.query.filter(peliculas.idPelicula==id_pelicula).first()
-    if pelicula is None:
-       print("No existe alguna pelicula con dicho ID") 
-    else:
-        pelicula.nombre= nombre
-        db.session.commit()
-        print("Se ha actualizado la pelicula con ID: "+ str(id_pelicula) + " a tener el nombre: "+ nombre)
-        
+# Función para actualizar una película por su ID
+def actualizar_pelicula(id, nombre=None, genero=None, duracion=None, inventario=None):
+    pelicula = peliculas.query.get(id)
+    if nombre:
+        pelicula.nombre = nombre
+    if genero:
+        pelicula.genero = genero
+    if duracion:
+        pelicula.duracion = duracion
+    if inventario:
+        pelicula.inventario = inventario
+    db.session.commit()
+    return pelicula
 
-
-def eliminar_id_pelicula(id_pelicula):
-    if not id_pelicula :
-        eliminar_tabla_peliculas()
-    else:
-        pelicula = peliculas.query.get(id_pelicula)
-        if pelicula is None:
-            print("No existe película con dicho ID.")
-        else:
-            db.session.delete(pelicula)
-            db.session.commit()
-            print(f"La película con ID: {id_pelicula} fue eliminada.")
-       
-
-def eliminar_tabla_peliculas():
-    confirmacion = input("Deseas eliminar todos los elementos de la tabla 'peliculas'? (Y/N): ")
-    if confirmacion == 'Y':
-        for pelicula in peliculas.query.all():
-            db.session.delete(pelicula)
-        db.session.commit()
-        print("Todos los elementos de la tabla 'peliculas' han sido eliminados.")
-    else:
-        print("No se han eliminado los elementos de la tabla 'peliculas'.")
+# Función para eliminar una película por su ID
+def eliminar_pelicula(id):
+    pelicula = peliculas.query.get(id)
+    db.session.delete(pelicula)
+    db.session.commit()
