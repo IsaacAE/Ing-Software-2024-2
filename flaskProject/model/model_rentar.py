29af2dc7c246a264fb01b9d6ac82dcc987d79010
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2024-03-12 23:50:09 -0600

    crearcion de operaciones CRU para rentas

--- flaskProject/model/model_rentar.py
+++ flaskProject/model/model_rentar.py
@@ -1,47 +1,34 @@
 from alchemyClasses.rentar import rentar
 from alchemyClasses import db
 
-def consultar_registros_rentas():
-    for renta in rentar.query.all():
-        print(renta)
+# Función para crear un registro de renta
+def crear_renta(idUsuario, idPelicula, fecha_renta, dias_de_renta=5, estatus=0):
+    nueva_renta = rentar(idUsuario=idUsuario, idPelicula=idPelicula, fecha_renta=fecha_renta, dias_de_renta=dias_de_renta, estatus=estatus)
+    db.session.add(nueva_renta)
+    db.session.commit()
+    return nueva_renta
 
-def filtrar_id_renta(id_renta):
-    renta = rentar.query.filter_by(idRentar=id_renta).first()
-    if renta is None:
-        print("No existe alguna renta con dicho ID.")
-    else:
-        print(renta)
+# Función para obtener todas las rentas
+def leer_rentas():
+    return rentar.query.all()
 
+# Función para obtener una renta por su ID
+def leer_renta_por_id(id):
+    return rentar.query.get(id)
 
-def cambiar_columna_fecha_renta(id_rentar,fecha):
-    renta = rentar.query.filter(rentar.idRentar==id_rentar).first()
-    if renta is None:
-       print("No existe alguna renta con dicho ID") 
-    else:
-        renta.fecha_renta= fecha
-        db.session.commit()
-        print("Se ha actualizado la renta con ID: "+ str(id_rentar) + " a tener la fecha de renta: "+ str(fecha))
-        
+# Función para actualizar una renta por su ID
+def actualizar_renta(id, idUsuario=None, idPelicula=None, fecha_renta=None, dias_de_renta=None, estatus=None):
+    renta = rentar.query.get(id)
+    if idUsuario:
+        renta.idUsuario = idUsuario
+    if idPelicula:
+        renta.idPelicula = idPelicula
+    if fecha_renta:
+        renta.fecha_renta = fecha_renta
+    if dias_de_renta:
+        renta.dias_de_renta = dias_de_renta
+    if estatus is not None:
+        renta.estatus = estatus
+    db.session.commit()
+    return renta
 
-
-def eliminar_id_renta(id_renta):
-    if not id_renta :
-        eliminar_tabla_rentar()
-    else:
-        renta = rentar.query.get(id_renta)
-        if renta is None:
-            print("No existe alguna renta con dicho ID.")
-        else:
-            db.session.delete(renta)
-            db.session.commit()
-            print(f"Usuario con ID {id_renta} eliminado exitosamente.")
-
-def eliminar_tabla_rentar():
-    confirmacion = input("Deseas eliminar todos los elementos de la tabla 'rentar'? (Y/N):")
-    if confirmacion == 'Y':
-        for renta in rentar.query.all():
-            db.session.delete(renta)
-        db.session.commit()
-        print("Todos los elementos de la tabla 'rentar' han sido eliminados.")
-    else:
-        print("No se han eliminado los elementos de la tabla 'rentar'.")
