13d47cc7db17df4720a7c86806d62a7b0bb067c5
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2024-03-14 03:17:21 -0600

    Operaciones CRU a para usuaio, falta Delete

--- flaskProject/contollers/ControllerUsuario.py
+++ flaskProject/contollers/ControllerUsuario.py
@@ -3,13 +3,38 @@ from model import model_usuarios as mu
 
 usuario_blueprint = Blueprint('usuario', __name__, url_prefix='/usuario')
 
+@usuario_blueprint.route('/buscarUsuario', methods=['GET', 'POST'])
+def mostrar_usuario_por_id():
+     if request.method == "GET":
+        return render_template('leer_usuarios.html')
+     else:
+
+        id = request.form["userId"]
+        usuario = mu.leer_usuario_por_id(id)
+        return render_template("mostrar_usuario.html", usuario=usuario)
+     
+@usuario_blueprint.route('/borrar', methods=['GET', 'POST'])
+def eliminar_usuario_por_id():
+     if request.method == "GET":
+        return render_template('borrar_usuarios.html')
+     else:
+
+        id = request.form["userId"]
+        print(id)
+        retorno = mu.eliminar_usuario(id)
+         
+        if retorno == -1:
+            return render_template("borrar_usuarios.html", mensaje="El correo ingresado ya existe, intente con otro correo", nombre = nombre, apellidoP = apellidoP, apellidoM = apellidoM, superuser = superuser, password = password)
+        else:
+            return render_template("user_added.html", mensaje="Usuario creado con éxito")
+
 
 @usuario_blueprint.route('/registro', methods=['GET', 'POST'])
 def agregar_usuario():
     if request.method == "GET":
-        return render_template('add_user.html')
+        return render_template('crear_usuario.html')
     else:
-        
+
         nombre = request.form["nombre"]
         print(nombre)
         apellidoP = request.form["apPat" ]
@@ -33,11 +58,43 @@ def agregar_usuario():
         if retorno == -1:
             return render_template("add_user.html", mensaje="El correo ingresado ya existe, intente con otro correo", nombre = nombre, apellidoP = apellidoP, apellidoM = apellidoM, superuser = superuser, password = password)
         else:
-            return render_template("add_user.html", mensaje="Usuario creado con éxito")
+            return render_template("user_added.html", mensaje="Usuario creado con éxito")
     
     
     
-@usuario_blueprint.route('/usuarios')
+@usuario_blueprint.route('/leerUsuarios')
 def mostrar_usuarios():
-    usuarios = mu.ver_usuarios()
-    return render_template("leer_usuarios.html", usuarios=usuarios)
+    usuarios = mu.leer_usuarios()
+    return render_template("mostrar_usuarios.html", usuarios=usuarios)
+
+@usuario_blueprint.route('/actualizar', methods=['GET', 'POST'])
+def actualizar_usuario():
+    if request.method == "GET":
+        return render_template('actualizar_usuario.html')
+    else:
+        id = request.form["userId"]
+        print(id)
+        nombre = request.form["nombre"]
+        print(nombre)
+        apellidoP = request.form["apPat" ]
+        print(apellidoP)
+        apellidoM = request.form["apMat"]
+        print(apellidoM)
+        correo = request.form["email"]
+        print(correo)
+        password = request.form["password"]
+        print(password)
+        
+        if 'superUser' in request.form:
+            superuser = 1
+        else:
+            superuser = 0
+        print(superuser)
+            
+        
+        retorno = mu.actualizar_usuario(id, nombre, apellidoP, password, apellidoM, correo, None, superuser)
+        
+        if retorno == -1:
+            return render_template("add_user.html", mensaje="El correo ingresado ya existe, intente con otro correo", nombre = nombre, apellidoP = apellidoP, apellidoM = apellidoM, superuser = superuser, password = password)
+        else:
+            return render_template("user_added.html", mensaje="Usuario creado con éxito")
--- flaskProject/model/model_usuarios.py
+++ flaskProject/model/model_usuarios.py
@@ -1,12 +1,17 @@
 from alchemyClasses.usuarios import usuarios
 from alchemyClasses import db
+from Cryptodome.Hash import SHA256
+from base64 import b64encode
 
 # Función para crear un nuevo usuario
 def crear_usuario(nombre, apPat, password, apMat=None, email=None, profilePicture=None, superUser=None):
     nuevo_usuario = usuarios(nombre=nombre, apPat=apPat, password=password, apMat=apMat, email=email, profilePicture=profilePicture, superUser=superUser)
-    db.session.add(nuevo_usuario)
-    db.session.commit()
-    return nuevo_usuario
+    try:
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+        return 0
+    except:
+        return -1
 
 
 
@@ -36,11 +41,19 @@ def actualizar_usuario(id, nombre=None, apPat=None, apMat=None, password=None, e
         usuario.profilePicture = profilePicture
     if superUser is not None:
         usuario.superUser = superUser
-    db.session.commit()
-    return usuario
+    try:
+        db.session.commit()
+        return 0
+    except:
+        return -1
 
 # Función para eliminar un usuario por su ID
 def eliminar_usuario(id):
+    
     usuario = usuarios.query.get(id)
-    db.session.delete(usuario)
-    db.session.commit()
+    try:
+       db.session.delete(usuario)
+       db.session.commit()
+       return 0  # Se encontró y eliminó el usuario correctamente
+    except:
+       return -1  # No se encontró el usuario
